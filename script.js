e2bc975a1ef49086adf557e5fc4ed4a11b678cd7
Gabirodrigueztj <gabirodriguez1d@outlook.com>
2023-10-31 16:32:25 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Agregar la clase de animación cuando la página esté completamente cargada
+document.addEventListener("DOMContentLoaded", function() {
+  const elContenido = document.querySelector("#elcontenido");
+  elContenido.classList.add("animate");
+  
+  // Agregar efecto de desplazamiento suave a los enlaces de navegación
+  const navLinks = document.querySelectorAll('.side-nav a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = document.querySelector(e.target.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
+
+// Agregar la clase de animación cuando la página esté completamente cargada
+document.addEventListener("DOMContentLoaded", function() {
+  const elContenido = document.querySelector("#elcontenido");
+  elContenido.classList.add("animate");
+
+  // Ejecutar estilo.css después de que animated.css termine de cargar y se aplique la animación
+  elContenido.addEventListener("animationend", function() {
+    elContenido.classList.remove("animate"); // Elimina la clase animate para que la animación no se repita al hacer hover
+  
+    
+  });
+});
